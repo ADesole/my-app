9cce1b591ed0950e8a8eb3858c07ae0e230308ae
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-05-10 00:55:32 +0300

    Completed task2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
+import productList from "./products";
 
+console.log(productList);
 function App() {
   return (
     <div className="App">
-      <h1>My bakery</h1>
+      <h1>My bakery </h1>
       <p>fun fact bread in spanish is "pan"</p>
       <img
         className="image1"
         src="https://www.bakingbusiness.com/ext/resources/2019/8/08192019/GlobalTrends.jpg?t=1566494557&width=1080"
         alt="Bakery"
       ></img>
+      {productList}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,38 @@
+const product = [
+  {
+    name: "Toast",
+    price: 1.5,
+    image:
+      "https://images.getrecipekit.com/20210919052613-breakfast-toast-topper-1030x687.jpg?width=650&quality=90&",
+  },
+  {
+    name: "baquette",
+    price: 3,
+    image:
+      "https://thumbs.dreamstime.com/b/hand-holding-french-baquette-bread-fresh-white-background-53713643.jpg",
+  },
+  {
+    name: "Hamburger Bun",
+    price: 1,
+    image:
+      "https://www.rockrecipes.com/wp-content/uploads/2021/06/The-Best-Hamburger-Buns-Recipe-close-up-photo-of-one-bun-split-in-half-horisontally.jpg",
+  },
+];
+const productList = product.map((prod) => {
+  return (
+    <div>
+      <img
+        src={prod.image}
+        alt="Bakery product"
+        style={{
+          width: "300px",
+          minHeight: "200px",
+        }}
+      ></img>
+      <h1>{prod.name}</h1>
+      <h2>{prod.price} KD</h2>
+    </div>
+  );
+});
+
+export default productList;
