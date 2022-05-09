eeb0a78cc47ff41a937015ab8a1d053f38f21b3c
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-05-09 23:31:22 +0300

    Complete app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My bakery</h1>
+      <p>fun fact bread in spanish is "pan"</p>
+      <img
+        className="image1"
+        src="https://www.bakingbusiness.com/ext/resources/2019/8/08192019/GlobalTrends.jpg?t=1566494557&width=1080"
+        alt="Bakery"
+      ></img>
     </div>
   );
 }
